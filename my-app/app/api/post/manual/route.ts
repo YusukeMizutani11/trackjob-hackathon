629891536a15873c068d3fdc45adfb41a6bd42a7
Bishwas Koirala <wasubisu69@gmail.com>
2024-06-30 00:15:54 +0900

    try catch

--- my-app/app/api/post/manual/route.ts
+++ my-app/app/api/post/manual/route.ts
@@ -33,6 +33,6 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json(interns, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ error: error }, { status: 500 });
+    return NextResponse.json({ error: error }, { status: 501 });
   }
 }
--- my-app/app/api/post/route.ts
+++ my-app/app/api/post/route.ts
@@ -9,6 +9,8 @@ const schema = z.object({
   event_name : z.string(),
   event_detail : z.string(),
   target_student : z.string(),
+  recruit_begin : z.string(),
+  recruit_end : z.string(),
   //TODO: nullable fields は入れてない
   tech_stack : z.string(),
   remote : z.string()
@@ -22,27 +24,34 @@ function convertToISODate(dateString : string) {
 
 export async function POST(req:NextRequest , res : NextResponse) {
 
-  const body = await req.json()
-  const validation = schema.safeParse(body)
-  if(!validation.success){
-    return NextResponse.json({error : validation.error.errors})
-  }
+  try{
 
-  const intern = await prisma.interns.create({
-    data : {
-      company : body.company,
-      url : body.url,
-      hiring : body.hiring,
-      event_name : body.event_name ,
-      event_detail : body.event_detail,
-      target_student : body.target_student,
-      tech_stack : body.tech_stack,
-      remote : body.remote
-      
+    const body = await req.json()
+    const validation = schema.safeParse(body)
+    if(!validation.success){
+      return NextResponse.json({error : validation.error.errors})
     }
-  })
-
-  return NextResponse.json(intern)
+    
+    const intern = await prisma.interns.create({
+      data : {
+        company : body.company,
+        url : body.url,
+        hiring : body.hiring,
+        event_name : body.event_name ,
+        event_detail : body.event_detail,
+        target_student : body.target_student,
+        recruit_begin : convertToISODate(body.recruit_begin),
+        recruit_end : convertToISODate(body.recruit_end),
+        tech_stack : body.tech_stack,
+        remote : body.remote
+        
+      }
+    })
+    
+    return NextResponse.json(intern)
+  } catch (error) {
+    return NextResponse.json({error : error} , {status : 501})
+  }
 }
 
 
