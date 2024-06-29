93d070452de2cd16345ceae40af3d6a2dbe2d062
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2024-06-29 17:33:50 +0900

    create table

--- my-app/app/components/FetchData.tsx
+++ my-app/app/components/FetchData.tsx
@@ -4,14 +4,34 @@ import { useFetch } from "../hooks/useFetchUser";
 const FetchData = () => {
   const { data } = useFetch("/api/get");
   return (
-    <div>
-      {data?.map((data) => (
-        <div key={data.id}>
-          <div>{data.company}</div>
-          <div>{data.remote}</div>
-        </div>
-      ))}
-    </div>
+	<div className="relative overflow-x-auto">
+		<table className="table-fixed bg-amber-100 border-separate border border-slate-500">
+			<thead className="text-red-200">
+				<tr className="border-separate border border-slate-500">
+					<th scope="col">Company</th>
+					<th scope="col">URL</th>
+					<th scope="col">Event Name</th>
+					<th scope="col">Target Student</th>
+					<th scope="col">Recruit Begin</th>
+					<th scope="col">Tech Stack</th>
+					<th scope="col">Remote</th>
+				</tr>
+			</thead>
+			<tbody>
+				{data?.map((data) => (
+					<tr key={data.key} className="border-separate border border-slate-500">
+						<td>{data.company}</td>
+						<td><a href={data.url}>{data.url}</a></td>
+						<td>{data.event_name}</td>
+						<td>{data.target_student}</td>
+						<td>{data.recruit_begin}</td>
+						<td>{data.tech_stack}</td>
+						<td>{data.remote}</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	</div>
   );
 };
 
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,9 +3,7 @@ import FetchData from "./components/FetchData";
 
 export default function Home() {
   return (
-    <main className="bg-white">
-      hello world
-
+	<main className="flex h-screen flex-col items-center p-24">
       <FetchData />
     </main>
   );
