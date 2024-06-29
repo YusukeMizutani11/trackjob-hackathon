cb3913013bc46a3f94c3a7b0e063758e8b452f6b
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2024-06-29 17:41:50 +0900

    fix table

--- my-app/app/components/FetchData.tsx
+++ my-app/app/components/FetchData.tsx
@@ -5,28 +5,28 @@ const FetchData = () => {
   const { data } = useFetch("/api/get");
   return (
 	<div className="relative overflow-x-auto">
-		<table className="table-fixed bg-amber-100 border-separate border border-slate-500">
+		<table className="table-fixed border-collapse border border-blue-200">
 			<thead className="text-red-200">
 				<tr className="border-separate border border-slate-500">
-					<th scope="col">Company</th>
-					<th scope="col">URL</th>
-					<th scope="col">Event Name</th>
-					<th scope="col">Target Student</th>
-					<th scope="col">Recruit Begin</th>
-					<th scope="col">Tech Stack</th>
-					<th scope="col">Remote</th>
+					<th scope="col" className="border border-gray-500 bg-blue-200">Company</th>
+					<th scope="col" className="border border-gray-500 bg-blue-200">URL</th>
+					<th scope="col" className="border border-gray-500 bg-blue-200">Event Name</th>
+					<th scope="col" className="border border-gray-500 bg-blue-200">Target Student</th>
+					<th scope="col" className="border border-gray-500 bg-blue-200">Recruit Begin</th>
+					<th scope="col" className="border border-gray-500 bg-blue-200">Tech Stack</th>
+					<th scope="col" className="border border-gray-500 bg-blue-200">Remote</th>
 				</tr>
 			</thead>
 			<tbody>
 				{data?.map((data) => (
 					<tr key={data.key} className="border-separate border border-slate-500">
-						<td>{data.company}</td>
-						<td><a href={data.url}>{data.url}</a></td>
-						<td>{data.event_name}</td>
-						<td>{data.target_student}</td>
-						<td>{data.recruit_begin}</td>
-						<td>{data.tech_stack}</td>
-						<td>{data.remote}</td>
+						<td className="border border-gray-500 bg-blue-200">{data.company}</td>
+						<td className="border border-gray-500 bg-blue-200"><a href={data.url}>{data.url}</a></td>
+						<td className="border border-gray-500 bg-blue-200">{data.event_name}</td>
+						<td className="border border-gray-500 bg-blue-200">{data.target_student}</td>
+						<td className="border border-gray-500 bg-blue-200">{data.recruit_begin}</td>
+						<td className="border border-gray-500 bg-blue-200">{data.tech_stack}</td>
+						<td className="border border-gray-500 bg-blue-200">{data.remote}</td>
 					</tr>
 				))}
 			</tbody>
