6f5811529823e90d4ba602b372be926e9eb41ef3
Bishwas Koirala <wasubisu69@gmail.com>
2024-06-30 15:03:10 +0900

    new page added

--- my-app/app/hooks/useFetchUser.ts
+++ my-app/app/hooks/useFetchUser.ts
@@ -1,8 +1,12 @@
+import { z } from "zod";
+import { schema } from "../api/post/route";
+
+type Data = z.infer<typeof schema>
 import { useState, useEffect } from "react";
 export const useFetch = (url: string) => {
-  const [data, setData] = useState<any[]>();
+  const [data, setData] = useState<Data[]>();
   const [isloading, setIsLoading] = useState(false);
-  const [error, setError] = useState<any>(null);
+  const [error, setError] = useState<string>("error");
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
@@ -10,7 +14,7 @@ export const useFetch = (url: string) => {
         const response = await fetch(url);
         const parsedData = await response.json();
         setData(parsedData);
-        setError(null);
+        setError("error");
       } catch {
         setError(`${error} could not fetch data`);
       }
