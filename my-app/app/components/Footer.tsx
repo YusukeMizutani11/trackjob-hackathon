2836d34ea4c8857b126d3b34e1127a6db46f7991
YusukeMizutani11 <yusuke3rdbee@gmail.com>
2024-06-30 14:28:10 +0900

    change the design of Footer

--- my-app/app/components/Footer.tsx
+++ my-app/app/components/Footer.tsx
@@ -5,9 +5,13 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex justify-center items-center h-24">
+    <footer className="flex justify-center items-center h-24 bg-black">
       <Link href="/form">
-        <Button size="xl" variant="outline" className="flex items-center justify-center h-full w-full">
+        <Button
+          size="xl"
+          variant="outline"
+          className="flex items-center justify-center h-12 px-8 text-lg font-semibold text-white bg-black border-2 border-white hover:bg-white hover:text-black shadow-md transform transition-transform duration-200 hover:scale-105 rounded-full"
+        >
           インターン情報の登録はこちら
         </Button>
       </Link>
